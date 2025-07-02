b65c22c17eea9b2074c076195e93225247df9cf8
Mincheol Ha <hmc0501@gmail.com>
2025-07-02 22:57:26 +0900

    secretKey 환경변수 설정으로 보안

--- src/main/java/com/example/mincheol1sr2/config/security/JwtTokenProvider.java
+++ src/main/java/com/example/mincheol1sr2/config/security/JwtTokenProvider.java
@@ -3,8 +3,10 @@ package com.example.mincheol1sr2.config.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,11 +24,19 @@ import java.security.Key;
 @RequiredArgsConstructor
 public class JwtTokenProvider {
 
-    private final Key secretKey = Keys.hmacShaKeyFor("SuperCodingFirstProjectSecretKey!!!!!##@@".getBytes(StandardCharsets.UTF_8));
+    @Value("${jwt.secret}")
+    private String secret;
 
     private long tokenValidMillisecond =  1000L * 60 * 60;
 
     private final UserDetailsService userDetailsService;
+
+    private Key key; // JWT에 실제로 사용하는 Key 객체
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
 
 
 
@@ -46,14 +56,14 @@ public class JwtTokenProvider {
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(new Date(now.getTime() + tokenValidMillisecond))
-                .signWith(secretKey, SignatureAlgorithm.HS256)
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public boolean validateToken(String jwtToken) {
         try {
             Claims claims = Jwts.parserBuilder()
-                    .setSigningKey(secretKey)
+                    .setSigningKey(key)
                     .build()
                     .parseClaimsJws(jwtToken)
                     .getBody();
@@ -71,7 +81,7 @@ public class JwtTokenProvider {
 
     private String getUserEmail(String jwtToken) {
         return Jwts.parserBuilder()
-                .setSigningKey(secretKey)
+                .setSigningKey(key)
                 .build()
                 .parseClaimsJws(jwtToken)
                 .getBody()
